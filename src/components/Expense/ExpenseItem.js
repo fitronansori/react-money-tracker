9fe300aa259b4d824d63fb797fe3041415467259
Fitron Ansori <fitronansori7@gmail.com>
2022-10-25 21:20:42 +0800

    folder structure

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item ">
+      <ExpenseDate date={props.date} className="expense-item__date" />
       <div className="expense-item__title">
-        <h1>{props.date.toISOString()}</h1>
-      </div>
-      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div>
           <span className="expense-item__price">${props.amount}</span>
