9cff47befc6f5af76e95898feb43d84a6df0dbe1
Fitron Ansori <fitronansori7@gmail.com>
2022-10-28 16:25:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
+import { useState } from "react";
 import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const Dummy_expenses = [
+  {
+    id: "e1",
+    title: "Tisu Toilet",
+    amount: 10.99,
+    date: new Date(2020, 7, 14),
+  },
+
+  {
+    id: "e2",
+    title: "TV Baru",
+    amount: 100.99,
+    date: new Date(2021, 2, 12),
+  },
+
+  {
+    id: "e3",
+    title: "Asuransi Mobil",
+    amount: 200.99,
+    date: new Date(2021, 2, 28),
+  },
+
+  {
+    id: "e4",
+    title: "Monitor Baru",
+    amount: 90.99,
+    date: new Date(2021, 5, 12),
+  },
+
+  {
+    id: "e5",
+    title: "Keyboard",
+    amount: 50.99,
+    date: new Date(2022, 10, 28),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Tisu Toilet",
-      amount: 10.99,
-      date: new Date(2020, 7, 14),
-    },
+  const [expenses, setExpenses] = useState(Dummy_expenses);
 
-    {
-      id: "e2",
-      title: "TV Baru",
-      amount: 100.99,
-      date: new Date(2021, 2, 12),
-    },
-
-    {
-      id: "e3",
-      title: "Asuransi Mobil",
-      amount: 200.99,
-      date: new Date(2021, 2, 28),
-    },
-
-    {
-      id: "e4",
-      title: "Monitor Baru",
-      amount: 90.99,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const selectChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filterByYear = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div className="expenses">
-      <ExpenseItem
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={selectChangeHandler}
+      />
+      {filterByYear.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      ;
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -24,7 +48,7 @@ function Expenses(props) {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </div>
   );
 }
